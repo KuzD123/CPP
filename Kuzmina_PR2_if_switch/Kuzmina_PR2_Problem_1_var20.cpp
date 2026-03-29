e40c1ed3450ced267568dcb0278372b13590fbc1
KuzD123 <dasha7391k@gmail.com>
2026-03-29 16:11:28 +0300

    3

---
+++ Kuzmina_PR2_if_switch/Kuzmina_PR2_Problem_1_var20.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float t;
+     cout <<"temperature celcius? - ";
+     cin >> t;
+     cout <<"temperature farenheight - " << (9/5)*t+32; 
+    return 0;
+}
